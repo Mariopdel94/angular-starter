3422bcb08326fe9f6c8d24e7f7c332764cb7c479
Mario Pineda <mariopdel94@gmail.com>
2020-03-19 13:03:24 -0600

    Added select. Updated forms css. Added snackbar documentation and examples. Updated snackbar css. Fixed colors style duplication. Fixed order on theme. Separated color classes to another file.

--- src/app/pages/styleguide/styleguide.module.ts
+++ src/app/pages/styleguide/styleguide.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { StyleguideRoutingModule } from './styleguide-routing.module';
 import { StyleguideComponent } from './styleguide.component';
 
 @NgModule({
   declarations: [StyleguideComponent],
-  imports: [CommonModule, FormsModule, StyleguideRoutingModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    StyleguideRoutingModule,
+    MatSnackBarModule
+  ]
 })
 export class StyleguideModule {}
